28e54e3abeac1efd2541d6bcf9dfde026dc0fc13
Artem_Plenkin <nice.plenkin@mail.ru>
2022-12-04 15:38:46 +0700

    final metaflow case

--- main.py
+++ main.py
@@ -4,19 +4,28 @@ from metaflow import FlowSpec, step
 class LinearFlow(FlowSpec):
 
     @step
-    def start(self):
+    def first(self):
         os.system('fastqc SRR8533688.fastq.gz')
-        self.my_var = 'hello world'
-        self.next(self.a)
+        self.next(self.second)
 
     @step
-    def a(self):
-        print('the data artifact is: %s' % self.my_var)
-        self.next(self.end)
+    def second(self):
+        os.system('bwa index ref_genome.fna')
+        self.next(self.third)
 
     @step
-    def end(self):
-        print('the data artifact is still: %s' % self.my_var)
+    def third(self):
+        os.system('bwa mem ref_genome.fna SSRR8533688.fastq.gz > result.sam')
+        self.next(self.forth)
+
+    @step
+    def third(self):
+        os.system('samtools flagstat result.sam > final')
+        self.next(self.fifth)
+
+    @step
+    def fifth(self):
+        os.system('python3 quality_check.py final')
 
 if __name__ == '__main__':
     LinearFlow()
