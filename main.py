c62bd4381edccc5d4be01939572e578b79bd5823
Artem_Plenkin <nice.plenkin@mail.ru>
2022-12-04 16:08:13 +0700

    final update

--- main.py
+++ main.py
@@ -6,20 +6,26 @@ class LinearFlow(FlowSpec):
     @step
     def start(self):
         os.system('fastqc SRR8533688.fastq.gz')
-        os.system('bwa index ref_genome.fna')
-        os.system('bwa mem ref_genome.fna SSRR8533688.fastq.gz > result.sam')
-        os.system('samtools flagstat result.sam > final')
-        os.system('python3 quality_check.py final')
         self.next(self.second)
 
     @step
     def second(self):
-        print('Next step')
+        os.system('bwa index ref_genome.fna')
+        self.next(self.third)
+
+    @step
+    def third(self):
+        os.system('bwa mem ref_genome.fna SRR8533688.fastq.gz > result.sam')
+        self.next(self.forth)
+
+    @step
+    def forth(self):
+        os.system('samtools flagstat result.sam > final')
         self.next(self.end)
 
     @step
     def end(self):
-       print('Finish')
+        os.system('python3 quality_check.py final')
 
 if __name__ == '__main__':
     LinearFlow()
