b3ae79d10bf54281296fae35b168e57f12d77713
Artem_Plenkin <nice.plenkin@mail.ru>
2022-12-04 16:02:33 +0700

    solve problem try

--- main.py
+++ main.py
@@ -6,26 +6,20 @@ class LinearFlow(FlowSpec):
     @step
     def start(self):
         os.system('fastqc SRR8533688.fastq.gz')
+        os.system('bwa index ref_genome.fna')
+        os.system('bwa mem ref_genome.fna SSRR8533688.fastq.gz > result.sam')
+        os.system('samtools flagstat result.sam > final')
+        os.system('python3 quality_check.py final')
         self.next(self.second)
 
     @step
     def second(self):
-        os.system('bwa index ref_genome.fna')
-        self.next(self.third)
-
-    @step
-    def third(self):
-        os.system('bwa mem ref_genome.fna SSRR8533688.fastq.gz > result.sam')
-        self.next(self.forth)
-
-    @step
-    def forth(self):
-        os.system('samtools flagstat result.sam > final')
+        print('Next step')
         self.next(self.end)
 
     @step
     def end(self):
-        os.system('python3 quality_check.py final')
+       print('Finish')
 
 if __name__ == '__main__':
     LinearFlow()
